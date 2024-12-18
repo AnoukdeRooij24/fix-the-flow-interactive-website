de53fc4dc591ef872a655dc5fcaf5f966705c2c4
AnoukdeRooij24 <anouk.de.rooij@hva.nl>
2024-12-18 11:54:58 +0100

    zonnetje toegevoegd

--- assets/main.js
+++ assets/main.js
@@ -6,14 +6,16 @@ let groeiButton = document.querySelector('button.groeispurt')
 
 // zoek de afbeelding van Harry die we willen laten groeien
 let harrry = document.querySelector('img.harry-img')
+let harrrySvg = document.querySelector('svg.harry-svg')
 
 // als je op de groeibutton klinkt dan moet harry groter worden
 groeiButton.addEventListener('click', grotermaken)
 
 // als de keyframe animatie klaar is dan wil ik harry weer kleiner maken
-harrry.addEventListener('animationend', grotermaken)
+// harrry.addEventListener('animationend', grotermaken)
 
 // de functie grotermaken moet de classname harry groeien eraf gehaald worden
 function grotermaken() {
   harrry.classList.toggle('harrygroeien');
+  harrrySvg.classList.toggle('wolkrechts');
 }
